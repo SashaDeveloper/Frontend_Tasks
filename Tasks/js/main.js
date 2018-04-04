f7dc77ae8938fb5f6e4fb1da86dae18fb355d6fc
Unknown <OliferchykSasha1998@gmail.com>
2018-04-04 12:09:16 +0300

    Add single page navigation.

---
+++ Tasks/js/main.js
@@ -0,0 +1,45 @@
+function on(_src, _alt) {
+
+    var searchedDiv = document.getElementById("overlay");
+    searchedDiv.innerHTML = null;
+    searchedDiv.style.display = "block";
+
+    var divHeader = document.createElement("div");
+
+    var imgClose = document.createElement("img");
+    imgClose.setAttribute("class", "img-close");
+    imgClose.setAttribute("src", "img/close-icon.png");
+    imgClose.setAttribute("alt", "Close button");
+    imgClose.setAttribute("onclick", "off()");
+
+    var h1HeaderText = document.createElement("h1");
+    h1HeaderText.appendChild(document.createTextNode(_alt));
+    divHeader.appendChild(imgClose);
+    divHeader.appendChild(h1HeaderText);
+    searchedDiv.appendChild(divHeader);
+
+    var imgShowed = document.createElement("img");
+    imgShowed.setAttribute("class", "img-default");
+    imgShowed.setAttribute("src", _src);
+    imgShowed.setAttribute("alt", _alt);
+    searchedDiv.appendChild(imgShowed);
+
+    addUrlToHistory(_src);
+}
+
+function addUrlToHistory(_src) {
+    window.history.pushState('Object', 'Title',"showPhoto.html");
+}
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
+}
+
+window.onpopstate = function (event) {
+    if (document.location.pathname == "/Tasks/index.html") {
+        off();
+    }
+    else {
+        document.getElementById("overlay").style.display = "block";
+    }
+}
